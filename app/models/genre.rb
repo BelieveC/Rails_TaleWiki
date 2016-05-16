0d25c988171f9a15f5e49cdbeab176fbc2435a55
Chaitanya Yadav <cayadav2009@gmail.com>
2016-05-17 00:41:51 +0530

    Genre model added with validations

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,5 @@
 class Genre < ActiveRecord::Base
 	has_many :stories
+	validates :name, presence: true,length:{minimum: 5,maximum: 25},uniqueness: true
+	validates :description, presence: true,length:{minimum: 25}
 end
