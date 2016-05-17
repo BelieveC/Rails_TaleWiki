2865dfd606c9612dcf75c5b45fa2c27a8ca37df5
Chaitanya Yadav <cayadav2009@gmail.com>
2016-05-17 07:34:54 +0530

    Story controller updated

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,47 @@
 class StoriesController < ApplicationController
+	def index
+		@stories = Story.all.order("created_at DESC")
+	end
+
+	def show
+	end
+
+	def new
+		@story = Story.new
+	end
+
+	def create
+		@story = Story.new(story_params)
+		if @story.save
+			redirect_to @story
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		@story.update(story_params)
+		if @story.save
+			redirect_to @story
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@story.destroy
+		redirect_to stories_path
+	end
+
+	private
+		def get_story
+			@story = Story.find(params[:id])
+		end
+
+		def story_params
+			params.require(:stories).permit(:source,:title,:body,:genre_id)
+		end
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -2,5 +2,5 @@ class Story < ActiveRecord::Base
 	belongs_to :genre
 	validates :title, presence: true, length:{maximum: 100,minimum: 1}
 	validates :source, presence: true,length:{maximum: 50,,minimum: 1}
-	validates
+	validates :body, presence: true, length:{minimum: 25}
 end
