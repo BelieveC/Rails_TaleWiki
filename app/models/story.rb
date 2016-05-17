21e7af4fc39a3ef772e46238bcb6195998d35a0d
Chaitanya Yadav <cayadav2009@gmail.com>
2016-05-17 07:06:25 +0530

    Validation added to story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,6 @@
 class Story < ActiveRecord::Base
 	belongs_to :genre
+	validates :title, presence: true, length:{maximum: 100,minimum: 1}
+	validates :source, presence: true,length:{maximum: 50,,minimum: 1}
+	validates
 end
